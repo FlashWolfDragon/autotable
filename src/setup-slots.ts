80f7a3c726578d3b4864f6ff79c867932d684b77
Flash <dom526@gmail.com>
2025-09-30 19:38:56 -0400

    Extra slot for meld

--- src/setup-slots.ts
+++ src/setup-slots.ts
@@ -324,11 +324,8 @@ export const SLOT_GROUPS: Record<GameType, Array<SlotGroup>> = {
     AMERICAN: [
     [start('hand'), row(14, undefined, {shift: true}), seats()],
     [start('hand.extra'), seats()],
-    [start('meld'), column(4), row(4, -Size.TILE.x, {push: true, shift: true}), seats()],
+    [start('meld'), column(4), row(5, -Size.TILE.x, {push: true, shift: true}), seats()],
     [start('wall'), row(19), stack(), seats()],
-    [start('washizu.bag'), stack(136), seats([0])],
-    [start('discard.washizu'), column(3, -Size.TILE.y), row(6, undefined, {push: true}), seats()],
-    [start('discard.washizu.extra'), row(4, undefined, {push: true}), seats()],
 
     [start('tray'), row(6, 24), column(10, -3), seats()],
     [start('payment'), row(8, 3), seats()],
